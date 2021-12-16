8844fa3d4ba427105d93a356605927bff6697b4f
Daniel Zolman <zolman.daniel@gmail.com>
2021-12-16 11:57:35 -0700

    added rollbar

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const path = require('path')
 
 const app = express()
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+
+var rollbar = new Rollbar({
+  accessToken: '383be7d16eb84a758f1ec8247e0d820e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname, '/public/index.html'))
+   rollbar.info("html file served succesfully")
 });
 
 const port = process.env.PORT || 4545
