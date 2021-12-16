e6021c9b721f85edb9c070c1952948e59e182b26
Daniel Zolman <zolman.daniel@gmail.com>
2021-12-16 11:38:40 -0700

    fixed port

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 4545
 
-app.listen(4545, () => console.log(`We're going to the future! Year ${port}`))
+app.listen(port, () => console.log(`We're going to the future! Year ${port}`))
